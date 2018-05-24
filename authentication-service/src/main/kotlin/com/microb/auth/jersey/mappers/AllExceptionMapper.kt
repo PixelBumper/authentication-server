eb20816c5dfaf0431bfccac2cd9020c344fbd64d
pixelbumper <PixelBumper@users.noreply.github.com>
2018-05-24 13:46:11 +0200

    made server error response a constant

--- authentication-service/src/main/kotlin/com/microb/auth/jersey/mappers/AllExceptionMapper.kt
+++ authentication-service/src/main/kotlin/com/microb/auth/jersey/mappers/AllExceptionMapper.kt
@@ -8,17 +8,20 @@ import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
 
+val INTERNAL_SERVER_ERROR = Response
+        .serverError()
+        .entity(ErrorMessageDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error"))
+        .build()!!
+
 @Provider
 @Component
 class AllExceptionMapper : ExceptionMapper<Throwable> {
 
     override fun toResponse(exception: Throwable): Response {
 
+
         LOG.error(exception.message, exception)
 
-        return Response
-                .serverError()
-                .entity(ErrorMessageDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error"))
-                .build()
+        return INTERNAL_SERVER_ERROR
     }
 }
