63a9019798658482dd7e1d09b42e74a497c6f0a1
pixelbumper <PixelBumper@users.noreply.github.com>
2018-05-24 13:42:43 +0200

    remapped validation exceptions to error responses containing an ErrprMessageDTO

--- authentication-service/src/main/kotlin/com/microb/auth/JerseyConfig.kt
+++ authentication-service/src/main/kotlin/com/microb/auth/JerseyConfig.kt
@@ -1,16 +1,21 @@
 package com.microb.auth
 
 import com.microb.auth.jersey.api.AccountApi
+import com.microb.auth.jersey.api.TokenApi
 import com.microb.auth.jersey.mappers.AllExceptionMapper
+import com.microb.auth.jersey.mappers.ValidationExceptionMapper
 import com.microb.auth.jersey.mappers.WebApplicationExceptionMapper
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource
+import org.glassfish.hk2.utilities.binding.AbstractBinder
 import org.glassfish.jersey.jackson.JacksonFeature
 import org.glassfish.jersey.server.ResourceConfig
 import org.glassfish.jersey.server.ServerProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory
 import org.springframework.stereotype.Component
+import javax.inject.Singleton
 import javax.ws.rs.ApplicationPath
+import javax.ws.rs.ext.ExceptionMapper
 
 const val JERSEY_BASE_PATH = "/api"
 
@@ -29,6 +34,17 @@ class JerseyConfig : ResourceConfig() {
     }
 
     private final fun configureJersey() {
+
+        register(object : AbstractBinder() {
+            override fun configure() {
+                bind(ValidationExceptionMapper::class.java)
+                        .to(ExceptionMapper::class.java)
+                        .`in`(Singleton::class.java)
+                        .ranked(10)
+            }
+
+        })
+
         // ensure non 200 responses are nor redirected to an error page
         property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true)
         // disable moxy
---
+++ authentication-service/src/main/kotlin/com/microb/auth/jersey/mappers/ValidationExceptionMapper.kt
@@ -0,0 +1,27 @@
+package com.microb.auth.jersey.mappers
+
+import com.microb.auth.jersey.dtos.ErrorMessageDTO
+import org.springframework.stereotype.Component
+import javax.validation.ConstraintViolationException
+import javax.validation.ValidationException
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.ExceptionMapper
+import javax.ws.rs.ext.Provider
+
+@Provider
+@Component
+class ValidationExceptionMapper: ExceptionMapper<ValidationException> {
+    override fun toResponse(exception: ValidationException?): Response {
+        return if (exception is ConstraintViolationException) {
+            val message = exception.constraintViolations.joinToString("\n") { it.message }
+            Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(
+                            ErrorMessageDTO(
+                                    Response.Status.BAD_REQUEST.statusCode,
+                                    message)).build()
+        } else {
+            INTERNAL_SERVER_ERROR
+        }
+    }
+}
