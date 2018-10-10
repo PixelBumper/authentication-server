389aa450a09578596b0b02d68e11340745a117f6
pixelbumper <PixelBumper@users.noreply.github.com>
2018-10-10 19:40:34 +0200

    fixed table name

--- authentication-model/src/main/kotlin/com/microb/auth/model/entities/CredentialIOSDevice.kt
+++ authentication-model/src/main/kotlin/com/microb/auth/model/entities/CredentialIOSDevice.kt
@@ -3,8 +3,10 @@ package com.microb.auth.model.entities
 import java.time.Instant
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.Table
 
 @Entity
+@Table(name = "credential_ios_device")
 class CredentialIOSDevice(
         account: Account,
         vendorId: String,
